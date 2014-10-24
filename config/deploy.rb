a6eb74330f3cd2b131d2ed8d38744f466e660721
Michael Lu <646179989@qq.com>
2014-10-24 22:45:45 +0800

    second commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,14 +2,14 @@ require 'mina/bundler'
 require 'mina/rails'
 require 'mina/git'
 # require 'mina/rbenv'  # for rbenv support. (http://rbenv.org)
-# require 'mina/rvm'    # for rvm support. (http://rvm.io)
+ require 'mina/rvm'    # for rvm support. (http://rvm.io)
 
 # Basic settings:
 #   domain       - The hostname to SSH to.
 #   deploy_to    - Path to deploy into.
 #   repository   - Git repo to clone from. (needed by mina/git)
 #   branch       - Branch name to deploy. (needed by mina/git)
-
+set :user, 'deploy'
 set :domain, 'ds1.gakky.net'
 set :deploy_to, '/var/www/mina'
 set :repository, 'git@github.com:luckylu/mina.git'
@@ -25,7 +25,7 @@ set :shared_paths, ['config/database.yml', 'log']
 # Optional settings:
 #   set :user, 'foobar'    # Username in the server to SSH to.
 #   set :port, '30000'     # SSH port number.
-#   set :forward_agent, true     # SSH forward_agent.
+   set :forward_agent, true     # SSH forward_agent.
 
 # This task is the environment that is loaded for most commands, such as
 # `mina deploy` or `mina rake`.
